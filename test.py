613476cb811bfa0109ad2b1692a34d1666bd5ed1
Jay <wenjay.du@gmail.com>
2020-01-06 15:55:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ model_path = 'path_of_your_h5_model'
 network_depth = 2  # default is 2, if you changed it while training, you should alter it
 input_shape = [29]  # [29] is the input shape of `credit card fraud` dataset, if you use other dataset, it may be diff
 
-x_test, y_test = dataLoading('dataset/creditcard_test.npy')
+x_test, y_test = dataLoading('dataset/creditcard_test.csv')
 
 scores = load_model_weight_predict(model_path,
                                    input_shape=input_shape,
